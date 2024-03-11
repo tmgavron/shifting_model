75a038e61f04118a826952b23085dde00e4eb8b0
tmgavron <you@example.com>
2024-03-10 19:11:32 -0500

    added more testing of average models

--- Visualization/VisualUtil.py
+++ Visualization/VisualUtil.py
@@ -25,8 +25,8 @@ MOUND = None
 FOULL = None
 FOULR = None
 ANGLE = None
-FIELD_HEIGHT = None
-FIELD_WIDTH  = None
+FIELD_HEIGHT = 5
+FIELD_WIDTH  = 5
 
 config = configparser.ConfigParser()
 config.read('Data//config.ini')

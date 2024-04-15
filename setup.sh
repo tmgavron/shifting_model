c5fa18594ac9497b678daa5543663bd095628666
Trent Gavron <tmgavron@gmail.com>
2024-04-15 08:27:52 -0500

    push new setup

--- setup.sh
+++ setup.sh
@@ -4,11 +4,18 @@
 # Install system-level dependencies
 if [ "$(uname)" == "Darwin" ]; then
     # macOS
-    brew install pkg-config cairo
+    brew install pkg-config cairo git-lfs
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Linux (Generic)
-    sudo apt-get install pkg-config libcairo2-dev  # Adjust as necessary for your distro
+    sudo apt-get update
+    sudo apt-get install pkg-config libcairo2-dev git-lfs
 fi
+
+# Initialize Git LFS
+git lfs install
 
 # Install Python dependencies
 pip install -r requirements.txt
+
+# Pull all LFS files
+git lfs pull
